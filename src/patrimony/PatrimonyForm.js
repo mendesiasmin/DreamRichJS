6e662784c9790b6e162ca59270c5bfb05fc926fd
Artur Bersan de Faria <artur_bersan@hotmail.com>
2017-09-09 17:50:16 -0300

    Puts paper on fgts

--- src/patrimony/PatrimonyForm.js
+++ src/patrimony/PatrimonyForm.js
@@ -3,37 +3,67 @@ import {Form} from 'formsy-react';
 import {FormsyText} from 'formsy-material-ui/lib';
 import {Auth} from '../auth/Auth';
 import patrimonyRoutes from '../routes/PatrimonyRoutes.js';
+import Paper from 'material-ui/Paper';
 
 export default class Employeer extends Component {
 
   submit(data){
+
+    var that = this;
+
     fetch(patrimonyRoutes[this.name],
       {
         method: 'POST',
         body: JSON.stringify(data),
         headers: Auth.getHeader(),
       }
-    ).then((e) => e.json())
-     .then((e) => console.log(e));
+    )
+    .then(((response) => {
+      var data = {};
+
+      if(response.ok) {
+        console.log(this.name + ' was submitted');
+        data = response.json();
+      } else {
+        throw new Error (this.name + ' could not be submitted');
+      }
+      return data;
+    }))
+    .then((data) => {
+      for (var form in that.forms) {
+        that.forms[form].inputs[0].setValue(data.id);
+        that.forms[form].submit();
+      }
+      console.log(data.id);
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
   }
 
   render() {
     return (
-      <Form onValidSubmit={this.submit}
-        onValid={() => true}
-        onInvalid={() => true}
-        onInvalidSubmit={() => console.log('Deu ruim')}
-        name="patrimony"
-      >
-        <FormsyText
-          name="fgts"
-          validations='isNumeric'
-          validationError='Esse campo precisa ser numérico'
-          hintText='O quanto você recebe de fgts?'
-          floatingLabelText='FGTS'
-        />
-        <button type='submit'>x</button>
-      </Form>
+      <div>
+        <h1> Registro de Patrimonio </h1>
+
+        <Paper className="Paper">
+          <Form onValidSubmit={this.submit}
+            onValid={() => true}
+            onInvalid={() => true}
+            onInvalidSubmit={() => console.log('Deu ruim')}
+            name="patrimony"
+          >
+            <FormsyText
+              name="fgts"
+              validations='isNumeric'
+              validationError='Esse campo precisa ser numérico'
+              hintText='O quanto você recebe de fgts?'
+              floatingLabelText='FGTS'
+            />
+            <button type='submit'>x</button>
+          </Form>
+        </Paper>
+      </div>
     );
   }
 }
